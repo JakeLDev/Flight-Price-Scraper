2550781454ba4c51d24094fb38c54a35828f366f
Jake Lyell <me@jakelyell.dev>
2022-05-31 18:40:07 +0800

    updates

--- Scraper.py
+++ Scraper.py
@@ -1,2 +1,9 @@
-import beautifulsoup4 as bs
+from bs4 import BeautifulSoup
+import requests
 
+# page = requests.get("https://dataquestio.github.io/web-scraping-pages/simple.html")
+page = requests.get("https://jakelyell.dev/")
+# print(page.content)
+
+soup = BeautifulSoup(page.content, 'html.parser')
+print(soup.prettify)
